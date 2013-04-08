5e5522587860dd59f282e90689ada6d02b3afd3e
Jamal Moubarak <jamalmoubarak@fullsail.edu>
2013-04-08 12:05:32 -0400

    Add initial repo files.

---
+++ js/json.js
@@ -0,0 +1,75 @@
+
+// Jamal Moubarak
+// Project 4
+// VFW 1303
+
+var json = {
+	"client1": {
+		"id": ["Client ID:", "12345678"],
+		"group": ["Install:", "POS"],
+		"compname": ["Company Name:", "Eskatech"],
+		"contname": ["Contact Name:", "Jamal Moubarak"],
+		"contphone": ["Contact Phone #", "734-819-1211"],
+		"contemail": ["Contact Email:", "jamal@eskatech.com"],
+		"date": ["Install Date:", "2013-03-14"],
+		"ipaddress": ["Ip Address:", "192.168.001.099"],
+		"sysuser": ["System Username:", "admin"],
+		"syspass": ["System Password:", "admin"],
+		"installed": ["The client has these systems installed:", "POS"],
+		"warranty": ["The client has this warranty:", "1 Year"],
+		"quanity": ["Quanity:", "4"],
+		"price": ["Price", "$4595.00"],
+		"notes": ["Notes:", "We installed 4 POS systems at the front counter off of a network switch we installed"]			
+	},
+	"client2":{
+		"id": ["Client ID:", "87654321"],
+		"group": ["Install:", "Surveillance"],
+		"compname": ["Company Name:", "Fairfield Inn"],
+		"contname": ["Contact Name:", "John Smith"],
+		"contphone": ["Contact Phone #", "555-555-2233"],
+		"contemail": ["Contact Email:", "john@gmail.com"],
+		"date": ["Install Date:", "2013-03-10"],
+		"ipaddress": ["Ip Address:", "192.168.001.100"],
+		"sysuser": ["System Username:", "john"],
+		"syspass": ["System Password:", "123456"],
+		"installed": ["The client has these systems installed:", "Surveillance"],
+		"warranty": ["The client has this warranty:", "3 Year"],
+		"quanity": ["Quanity:", "32"],
+		"price": ["Price", "$7995.00"],
+		"notes": ["Notes:", "We installed 8 cams 1st floor, 8 2nd floor, 8 3rd floor, 8 outside"]
+	},
+	"client3":{
+		"id": ["Client ID:", "23456789"],
+		"group": ["Install:", "Network"],
+		"compname": ["Company Name:", "Del Taco"],
+		"contname": ["Contact Name:", "Jane Smith"],
+		"contphone": ["Contact Phone #", "555-555-1111"],
+		"contemail": ["Contact Email:", "jane@gmail.com"],
+		"date": ["Install Date:", "2012-02-30"],
+		"ipaddress": ["Ip Address:", "192.168.001.99"],
+		"sysuser": ["System Username:", "jane"],
+		"syspass": ["System Password:", "deltaco"],
+		"installed": ["The client has these systems installed:", "Network"],
+		"warranty": ["The client has this warranty:", "3 Year"],
+		"quanity": ["Quanity:", "16"],
+		"price": ["Price", "$10995.00"],
+		"notes": ["Notes:", "We installed 16 cams and a sonic firewall for free wi-fi"]
+	},
+	"client4":{
+		"id": ["Client ID:", "98765432"],
+		"group": ["Install:", "AudioVideo"],
+		"compname": ["Company Name:", "Petes Garage"],
+		"contname": ["Contact Name:", "Pete George"],
+		"contphone": ["Contact Phone #", "555-555-2222"],
+		"contemail": ["Contact Email:", "pete@gmail.com"],
+		"date": ["Install Date:", "2013-01-24"],
+		"ipaddress": ["Ip Address:", "192.168.000.99"],
+		"sysuser": ["System Username:", "guest"],
+		"syspass": ["System Password:", "123456"],
+		"installed": ["The client has these systems installed:", "Audio / Video"],
+		"warranty": ["The client has this warranty:", "90 Days"],
+		"quanity": ["Quanity:", "4"],
+		"price": ["Price", "$3995.00"],
+		"notes": ["Notes:", "We installed 4 speakers in the customer area and install a password protected linksys router so they can give wifi to customers."]
+	}
+}
